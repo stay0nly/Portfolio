0365c2fda12cd8d378895d1decb505764ae7b385
stay0nly <derkach.dmt@gmail.com>
2022-06-03 13:41:01 +0800

    Sendmail fixed

--- app.js
+++ app.js
@@ -58,15 +58,81 @@ function closeModal(modal) {
 };
 
 
- 
-
-
 // Form sending
+const alertSuccess = document.getElementById('alert-success');
+const alertError = document.getElementById('alert-error');
+const form = document.getElementById('form');
+form.addEventListener('submit', formSend);
 
+async function formSend(e) {
+    e.preventDefault();
 
+    let error = formValidate(form);
 
-
-    // Test email function
-    function emailTest(input) {
-        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    let formData = new FormData(form);
+    
+    if (error === 0) {
+        form.reset();
+        alertSuccess.classList.add('active');
+        setTimeout(function(){alertSuccess.classList.remove('active');}, 3000);        
+        let response = await fetch('sendmail.php', {
+            method: 'POST',
+            body: formData
+        });
+        if(response.ok) {
+            let result = await response.json();
+            alert(result.message);
+            form.reset();            
+            alertSuccess.classList.add('active');
+            setTimeout(function(){alertSuccess.classList.remove('active');}, 3000);                        
+        } else {
+            alert("Error");            
+            alertError.classList.add('active');
+            setTimeout(function(){alertError.classList.remove('active');}, 3000);
+        }
+    } else {
+        alert("Fill all the fields.");
+        alertError.classList.add('active');
+        setTimeout(function(){alertError.classList.remove('active');}, 3000);
     }
+}
+
+function formValidate(form) {
+    let error = 0;
+    let fromReq = document.querySelectorAll('._req');
+
+    for (let index = 0; index < fromReq.length; index++) {
+        const input = fromReq[index];
+        fromRemoveError(input);
+        
+        if(input.classList.contains('._email')) {
+            if(emailTest(input)) {
+                fromAddError(input);
+                error++;
+            }
+        } else {
+            if (input.value === '') {
+                fromAddError(input);
+                error++;
+            }
+        }
+    }
+    return error;
+}
+
+function fromAddError(input) {
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+}
+function fromRemoveError(input) {
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+}
+
+// Test email function
+function emailTest(input) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
+
+
+    
